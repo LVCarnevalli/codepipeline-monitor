d3ee2180615980c9dac538f243fe6861d33d8e65
Lucas Carnevalli <lucasvc@ciandt.com>
2017-08-20 14:46:46 -0300

    Find actions pipeline

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -11,18 +11,22 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
   const pipelineStages = document.querySelectorAll('.pipelines-view-stages-list>div');
   if (pipelineStages) {
     pipelineStages.forEach(function (stage) {
+      const pipelineActions = stage.querySelectorAll('.pipelines-view-action');
       const buildType = stage.querySelectorAll('.pipelines-view-stage-name')[0].outerText;
-      const buildName = stage.querySelectorAll('.pipelines-view-action-name')[0].outerText;
-      const buildStatus = stage.querySelectorAll('.pipelines-view-action-state-status')[0].outerText;
-      const buildLastExecution = stage.querySelectorAll('.pipelines-view-action-state-timestamp')[0].outerText;
 
-      pipeline.stages.push({
-        type: buildType,
-        name: buildName,
-        status: buildStatus,
-        lastExecution: buildLastExecution
+      pipelineActions.forEach(function (action) {
+        const buildName = action.querySelectorAll('.pipelines-view-action-name')[0].outerText;
+        const buildStatus = action.querySelectorAll('.pipelines-view-action-state-status')[0].outerText;
+        const buildLastExecution = action.querySelectorAll('.pipelines-view-action-state-timestamp')[0].outerText;
+
+        pipeline.stages.push({
+          type: buildType,
+          name: buildName,
+          status: buildStatus,
+          lastExecution: buildLastExecution
+        });
       });
-    })
+    });
     sendResponse(pipeline);
   }
 });
